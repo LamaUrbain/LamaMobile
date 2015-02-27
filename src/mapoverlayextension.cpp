3eb591d60adbdaaec268b78ff6eec0640672c093
Louis du Verdier <louis.du.verdier@free.fr>
2015-02-27 16:02:45 +0000

    Implementation of the indicators (two things to fix: it is cut when a tile comes above it, and it is not as precise as it could be).

--- src/mapoverlayextension.cpp
+++ src/mapoverlayextension.cpp
@@ -1,7 +1,13 @@
+#include <cmath>
+#include <QPixmap>
 #include "mapoverlayextension.h"
 
+static const int indicatorHalfWidth = 24;
+static const int indicatorHeight = 48;
+
 MapOverlayExtension::MapOverlayExtension(MapWidget *map)
-    : MapExtension(map)
+    : MapExtension(map),
+      _indicator(":/images/map_indicator.png")
 {
 }
 
@@ -24,6 +30,64 @@ void MapOverlayExtension::removeTile(const MapTile &tile)
     _map->repaint();
 }
 
+const QList<QPointF> &MapOverlayExtension::getPoints() const
+{
+    return _points;
+}
+
+void MapOverlayExtension::appendPoint(const QPointF &coords)
+{
+    _points.append(coords);
+    onTilesChanged();
+}
+
+void MapOverlayExtension::prependPoint(const QPointF &coords)
+{
+    _points.prepend(coords);
+    onTilesChanged();
+}
+
+void MapOverlayExtension::insertPoint(int pos, const QPointF &coords)
+{
+    _points.insert(pos, coords);
+    onTilesChanged();
+}
+
+void MapOverlayExtension::movePoint(int pos, const QPointF &coords)
+{
+    _points[pos] = coords;
+    onTilesChanged();
+}
+
+void MapOverlayExtension::removePoint(int pos)
+{
+    _points.removeAt(pos);
+    onTilesChanged();
+}
+
+void MapOverlayExtension::onTilesChanged()
+{
+    for (quint8 i = 0; i < 20; ++i)
+        _tilePoints[i].clear();
+    _map->repaint();
+}
+
+void MapOverlayExtension::generateTilePoints(quint8 scale)
+{
+    int tilesNumber = pow(2.0, scale);
+
+    for (QList<QPointF>::const_iterator it = _points.constBegin(); it != _points.constEnd(); ++it)
+    {
+        const QPointF &coords = *it;
+
+        QPointF pos;
+        pos.setX((coords.x() + 180.0) * tilesNumber / 360.0);
+        pos.setY((1.0 - log(tan(coords.y() * M_PI / 180.0) + 1.0 / cos(coords.y() * M_PI / 180.0)) / M_PI) / 2.0 * tilesNumber);
+
+        _tilePoints[scale].insert(QPoint((int)pos.x(), (int)pos.y()), pos);
+    }
+}
+
 void MapOverlayExtension::begin(QPainter *painter)
 {
     Q_UNUSED(painter);
@@ -31,12 +95,30 @@ void MapOverlayExtension::begin(QPainter *painter)
 
 void MapOverlayExtension::drawTile(QPainter *painter, const QPoint &pos, const QPoint &tilePos)
 {
-    const QHash<QPoint, MapTile>::const_iterator it = _tiles[_map->getMapScale()].find(pos);
+    quint8 scale = _map->getMapScale();
 
-    if (it != _tiles[_map->getMapScale()].end())
+    if (_tilePoints[scale].size() != _points.size())
+        generateTilePoints(scale);
+
+    QHash<QPoint, MapTile>::const_iterator it = _tiles[scale].constFind(pos);
+    QMultiHash<QPoint, QPointF>::const_iterator pit = _tilePoints[scale].constFind(pos);
+
+    if (it != _tiles[scale].end())
     {
         const MapTile &tile = it.value();
         painter->drawPixmap(tilePos, tile.pixmap);
+    }
+
+    while (pit != _tilePoints[scale].constEnd() && pit.key() == pos)
+    {
+        const QPointF &pos = *pit;
+
+        int xOffset = qBound<qreal>(0, pos.x() - (int)pos.x(), 1) * 256 - indicatorHalfWidth;
+        int yOffset = qBound<qreal>(0, pos.y() - (int)pos.y(), 1) * 256 - indicatorHeight;
+
+        painter->drawPixmap(tilePos.x() + xOffset, tilePos.y() + yOffset, _indicator);
+
+        ++pit;
     }
 }
 
--- src/mapoverlayextension.h
+++ src/mapoverlayextension.h
@@ -1,6 +1,7 @@
 #ifndef MAPOVERLAYEXTENSION_H
 #define MAPOVERLAYEXTENSION_H
 
+#include <QMultiHash>
 #include "mapwidgetprivate.h"
 #include "mapextension.h"
 
@@ -13,12 +14,26 @@ public:
     void addTile(const MapTile &tile);
     void removeTile(const MapTile &tile);
 
+    const QList<QPointF> &getPoints() const;
+    void appendPoint(const QPointF &coords);
+    void prependPoint(const QPointF &coords);
+    void insertPoint(int pos, const QPointF &coords);
+    void movePoint(int pos, const QPointF &coords);
+    void removePoint(int pos);
+
     virtual void begin(QPainter *painter);
     virtual void drawTile(QPainter *painter, const QPoint &pos, const QPoint &tilePos);
     virtual void end(QPainter *painter);
 
 private:
+    void onTilesChanged();
+    void generateTilePoints(quint8 scale);
+
+private:
     QHash<QPoint, MapTile> _tiles[20];
+    QMultiHash<QPoint, QPointF> _tilePoints[20];
+    QList<QPointF> _points;
+    QPixmap _indicator;
 };
 
 #endif // MAPOVERLAYEXTENSION_H
