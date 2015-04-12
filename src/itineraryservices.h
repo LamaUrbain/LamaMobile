7d548e3a8cf21cbd8703aab22730fce495067569
Louis du Verdier <louis.du.verdier@free.fr>
2015-04-12 17:58:30 +0200

    Accessibility of the services through context properties.

--- src/itineraryservices.h
+++ src/itineraryservices.h
@@ -6,10 +6,13 @@
 
 class ItineraryServices : public ServicesBase
 {
+    Q_OBJECT
+
 public:
     ItineraryServices();
     virtual ~ItineraryServices();
 
+public slots:
     void createItinerary(QVariantList points, QJSValue callback); // TODO: settings
     void deleteItinerary(int id, QJSValue callback);
     void getItinerary(int id, int zoomLevel, QJSValue callback);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include "mobilitydiagram.h"
 #include "mapgetter.h"
 #include "mapwidget.h"
+#include "itineraryservices.h"
+#include "userservices.h"
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +15,12 @@ int main(int argc, char *argv[])
     qmlRegisterType<MapGetter>("MapControls", 1, 0, "MapGetter");
     qmlRegisterType<MapWidget>("MapControls", 1, 0, "MapWidget");
 
+    UserServices userServices;
+    ItineraryServices itineraryServices;
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("userServices", &userServices);
+    engine.rootContext()->setContextProperty("itineraryServices", &itineraryServices);
     engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
 
     return app.exec();
