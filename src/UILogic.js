3f04c66e14e6e6449ee72ef2e9cbcbff9a4fba72
Vihess <v.dos.santos@outlook.com>
2015-12-10 00:16:59 +0100

    Adding warning when destination is too far

--- src/UILogic.js
+++ src/UILogic.js
@@ -70,9 +70,15 @@ function updateItinerary(obj)
             if (destination.address)
                 addr = destination.address;
             else if ("latitude" in destination && "longitude" in destination)
+            {
                 addr = destination.latitude + "," + destination.longitude;
+            }
             if (addr)
+            {
                 rootView.currentItinerary.destinations.append({ address: addr });
+                checkDistance(i == 0 ? rootView.currentItinerary.departure : rootView.currentItinerary.destinations[i - 1],
+                            addr, destination.address ? destination.address : null)
+            }
         }
     }
 
@@ -289,5 +295,71 @@ function displayEvent(events, parentMap)
         console.debug("placing event", event.name, "at", event.position.latitude, ",", event.position.longitude)
         var component = Qt.createComponent("qrc:/Components/EventPoints.qml")
         component.createObject(parentMap, attrs)
+    }
+}
+
+function showDistanceWarningModal(address, distance)
+{
+    if (rootView.modal.visible == true && rootView.modal.title === "Watchout !")
+        address = "a few addresses of wich some are\n" +
+                   " further than 20 Kms away"
+    else
+    {
+        rootView.modal.title = "Watchout !";
+        rootView.modal.loading = false;
+        address = "an address ";
+        if (address !== null && address.length > 0)
+            address += ":\n" + address + "\n";
+        else
+            address += " has been found ";
+        address += Math.round(distance) + "Kms away."
+    }
+    rootView.modal.message = "We found " + address + "\n"
+                            + "You might want to give us additional informations \n"
+                            + "for enhanced results.";
+    rootView.modal.visible = true;
+}
+
+function getDistance(A_coords, B_coords)
+{
+    function toRad(deg) { return (deg * (Math.PI / 180)); }
+
+    var dLat = toRad(B_coords.latitude - A_coords.latitude)
+    var dLon = toRad(B_coords.longitude - A_coords.longitude)
+    var ALat = toRad(A_coords.latitude);
+    var BLat = toRad(B_coords.latitude);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(ALat) * Math.cos(BLat);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return (c * 6371); // 6371 = earth in km
+}
+
+function checkDistance(A, B, displayAddress, IsAddr_A, IsAddr_B)
+{
+    if (A == null || B == null)
+        return;
+    if (typeof(IsAddr_A) === "undefined")
+        IsAddr_A = Api.isLatLong(A) === null
+    if (typeof(IsAddr_B) === "undefined")
+        IsAddr_B = Api.isLatLong(B) === null
+
+    if (IsAddr_A || IsAddr_B)
+    {
+        var X = IsAddr_A ? A : B
+        Api.geocode(X, function (result)
+        {
+            if (X == A && IsAddr_B)
+                checkDistance(result, B, displayAddress, false, IsAddr_B)
+            else if (X == B)
+                checkDistance(A, result, displayAddress, IsAddr_A, false)
+        });
+    }
+    else
+    {
+        var distance_km = getDistance(A, B);
+        if (distance_km > 20)
+            showDistanceWarningModal(typeof(displayAddress) === "undefined" ? null : displayAddress,
+                                    distance_km);
     }
 }
